f799dd3e3f688b2795e194ceea5aa241d32d1b74
Pratim Ugale <pratim.ugale@gmail.com>
2018-06-26 23:14:40 +0530

    matplotlib added

--- linear_regression.py
+++ linear_regression.py
@@ -1,2 +1,7 @@
-import matplotlib
+import matplotlib.pyplot as plt
 
+plt.plot([1,2,3,4], [0,2,32,0], 'ro', markersize = 2)
+plt.axis([0, 100, 0, 100])
+plt.xlabel('X - AXIS')
+plt.ylabel('Y - AXIS')
+plt.show()
